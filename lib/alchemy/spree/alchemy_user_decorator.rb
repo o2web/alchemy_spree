80a4ddf01c14732a738297cb77274c86551a9721
Kevin Giguère <kgiguere@o2web.ca>
2015-07-03 17:05:36 -0400

    bug reload

--- lib/alchemy/spree/alchemy_user_decorator.rb
+++ lib/alchemy/spree/alchemy_user_decorator.rb
@@ -1,10 +1,11 @@
-Alchemy::User.class_eval do
-
-  def spree_roles
-    if admin?
-      ::Spree::Role.where(name: 'admin')
-    else
-      ::Spree::Role.where('1 = 0') # aka. empty relation
+Rails.configuration.to_prepare do
+  Alchemy::User.class_eval do
+    def spree_roles
+      if admin?
+        ::Spree::Role.where(name: 'admin')
+      else
+        ::Spree::Role.where('1 = 0') # aka. empty relation
+      end
     end
   end
 end
--- lib/alchemy/spree/spree_user_decorator.rb
+++ lib/alchemy/spree/spree_user_decorator.rb
@@ -1,10 +1,11 @@
-Spree::User.class_eval do
-
-  def alchemy_roles
-    if has_spree_role?(:admin)
-      %w(admin)
-    else
-      []
+Rails.configuration.to_prepare do
+  Spree::User.class_eval do
+    def alchemy_roles
+      if has_spree_role?(:admin)
+        %w(admin)
+      else
+        []
+      end
     end
   end
 end
